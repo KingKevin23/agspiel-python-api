b93984f51e87df27c081815dcd4773a8ef02402c
KingKevin23 <kevin.baier@protonmail.com>
2020-08-09 01:14:06 +0200

    fixed dependencies

--- agspiel/__init__.py
+++ agspiel/__init__.py
@@ -1,2 +1,3 @@
 #  Copyright (c) 2020 | KingKevin23 (@kingkevin023)
 
+import agspiel.api, agspiel.utils
--- agspiel/utils/__init__.py
+++ agspiel/utils/__init__.py
@@ -1,2 +1,3 @@
 #  Copyright (c) 2020 | KingKevin23 (@kingkevin023)
 
+from agspiel.utils import main
