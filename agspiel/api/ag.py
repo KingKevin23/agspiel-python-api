b41a40fc5332f09652ff380b6b2fcb6c66e12b74
KingKevin23 <kevin.baier@protonmail.com>
2020-08-10 17:18:08 +0200

    changed Ag structure

--- agspiel/api/ag.py
+++ agspiel/api/ag.py
@@ -8,281 +8,239 @@ from .zertifikat import Zertifikat
 from .order import Order
 
 class Ag:
-    def __init__(self, wkn:int, name:str, gruendung:datetime, aktienanzahl:int, in_liquidation:bool, kurs:float,
-                 brief:float, geld:float, brief_stueckzahl:int, geld_stueckzahl:int, depotwert:float, bargeld:float,
-                 highscore:int, highscore_groesse:int, highscore_wachstum:int, highscore_newcomer:int,
-                 agsx_punkte:int, in_agsx:bool, handelsaktivitaet:int, ceo:Ceo, aktien:list, anleihen:list,
-                 kredite:list, zertifikate:list, orders:list):
-        self.wkn:int = wkn
-        self.name:str = name
-        self.gruendung:datetime = gruendung
-        self.aktienanzahl:int = aktienanzahl
-        self.in_liquidation:bool = in_liquidation
-        self.kurs:float = kurs
-        self.brief:float = brief
-        self.geld:float = geld
-        self.brief_stueckzahl:int = brief_stueckzahl
-        self.geld_stueckzahl:int = geld_stueckzahl
-        self.depotwert:float= depotwert
-        self.bargeld:float = bargeld
-        self.highscore:int = highscore
-        self.highscore_groesse:int = highscore_groesse
-        self.highscore_wachstum:int = highscore_wachstum
-        self.highscore_newcomer:int = highscore_newcomer
-        self.agsx_punkte:int = agsx_punkte
-        self.in_agsx:bool = in_agsx
-        self.handelsaktivitaet:int = handelsaktivitaet
-        self.ceo:Ceo = ceo
-        self.aktien:list = aktien
-        self.anleihen:list = anleihen
-        self.kredite:list = kredite
-        self.zertifikate:list= zertifikate
-        self.orders:list = orders
+    def __init__(self, api_data:dict, web_data:str):
+        self._api_data:dict = api_data
+        self._web_data:str = web_data
+        self._wkn:int = None
+        self._name:str = None
+        self._gruendung:datetime = None
+        self._aktienanzahl:int = None
+        self._in_liquidation:bool = None
+        self._kurs:float = None
+        self._brief:float = None
+        self._geld:float = None
+        self._brief_stueckzahl:int = None
+        self._geld_stueckzahl:int = None
+        self._depotwert:float = None
+        self._bargeld:float = None
+        self._highscore:int = None
+        self._highscore_groesse:int = None
+        self._highscore_wachstum:int = None
+        self._highscore_newcomer:int = None
+        self._agsx_punkte:int = None
+        self._in_agsx:bool = None
+        self._handelsaktivitaet:int = None
+        self._ceo:Ceo = None
+        self._aktien:list = None
+        self._anleihen:list = None
+        self._kredite:list = None
+        self._zertifikate:list = None
+        self._orders:list = None
 
     @property
     def wkn(self) -> int:
-        return self._wkn
+        if self._wkn is None:
+            self._wkn = int(self._api_data.get("wkn"))
 
-    @wkn.setter
-    def wkn(self, value:int):
-        self._wkn = value
+        return self._wkn
 
     @property
     def name(self) -> str:
-        return self._name
+        if self._name is None:
+            self._name = self._api_data.get("name")
 
-    @name.setter
-    def name(self, value:str):
-        self._name = value
+        return self._name
 
     @property
     def gruendung(self) -> datetime:
-        return self._gruendung
+        if self._gruendung is None:
+            self._gruendung = datetime.strptime(self._api_data.get("gruendung"), "%Y-%m-%d %H:%M:%S")
 
-    @gruendung.setter
-    def gruendung(self, value:datetime):
-        self._gruendung = value
+        return self._gruendung
 
     @property
     def aktienanzahl(self) -> int:
-        return self._aktienanzahl
+        if self._aktienanzahl is None:
+            self._aktienanzahl = int(self._api_data.get("aktienanzahl"))
 
-    @aktienanzahl.setter
-    def aktienanzahl(self, value:int):
-        self._aktienanzahl = value
+        return self._aktienanzahl
 
     @property
     def in_liquidation(self) -> bool:
-        return self._in_liquidation
+        if self._in_liquidation is None:
+            self._in_liquidation = self._api_data.get("in_liquidation") == "true"
 
-    @in_liquidation.setter
-    def in_liquidation(self, value:bool):
-        self._in_liquidation = value
+        return self._in_liquidation
 
     @property
     def kurs(self) -> float:
-        return self._kurs
+        if self._kurs is None:
+            self._kurs = float(self._api_data.get("kurs"))
 
-    @kurs.setter
-    def kurs(self, value:float):
-        self._kurs = value
+        return self._kurs
 
     @property
     def brief(self) -> float:
-        return self._brief
+        if self._brief is None:
+            self._brief = float(self._api_data.get("brief"))
 
-    @brief.setter
-    def brief(self, value:float):
-        self._brief = value
+        return self._brief
 
     @property
     def geld(self) -> float:
-        return self._geld
+        if self._geld is None:
+            self._geld = float(self._api_data.get("geld"))
 
-    @geld.setter
-    def geld(self, value:float):
-        self._geld = value
+        return self._geld
 
     @property
     def brief_stueckzahl(self) -> int:
-        return self._brief_stueckzahl
+        if self._brief_stueckzahl is None:
+            self._brief_stueckzahl = int(self._api_data.get("brief_stueckzahl"))
 
-    @brief_stueckzahl.setter
-    def brief_stueckzahl(self, value:int):
-        self._brief_stueckzahl = value
+        return self._brief_stueckzahl
 
     @property
     def geld_stueckzahl(self) -> int:
-        return self._geld_stueckzahl
+        if self._geld_stueckzahl is None:
+            self._geld_stueckzahl = int(self._api_data.get("geld_stueckzahl"))
 
-    @geld_stueckzahl.setter
-    def geld_stueckzahl(self, value:int):
-        self._geld_stueckzahl = value
+        return self._geld_stueckzahl
 
     @property
     def depotwert(self) -> float:
-        return self._depotwert
+        if self._depotwert is None:
+            self._depotwert = float(self._api_data.get("depotwert"))
 
-    @depotwert.setter
-    def depotwert(self, value:float):
-        self._depotwert = value
+        return self._depotwert
 
     @property
     def bargeld(self) -> float:
-        return self._bargeld
+        if self._bargeld is None:
+            self._bargeld = float(self._api_data.get("bargeld"))
 
-    @bargeld.setter
-    def bargeld(self, value:float):
-        self._bargeld = value
+        return self._bargeld
 
     @property
     def highscore(self) -> int:
-        return self._highscore
+        if self._highscore is None:
+            self._highscore = int(self._api_data.get("highscore_platz"))
 
-    @highscore.setter
-    def highscore(self, value:int):
-        self._highscore = value
+        return self._highscore
 
     @property
     def highscore_groesse(self) -> int:
-        return self._highscore_groesse
+        if self._highscore_groesse is None:
+            self._highscore_groesse = int(self._api_data.get("highscore_platz_groesse"))
 
-    @highscore_groesse.setter
-    def highscore_groesse(self, value:int):
-        self._highscore_groesse = value
+        return self._highscore_groesse
 
     @property
     def highscore_wachstum(self) -> int:
-        return self._highscore_wachstum
+        if self._highscore_wachstum is None:
+            self._highscore_wachstum = int(self._api_data.get("highscore_platz_wachstum"))
 
-    @highscore_wachstum.setter
-    def highscore_wachstum(self, value:int):
-        self._highscore_wachstum = value
+        return self._highscore_wachstum
 
     @property
     def highscore_newcomer(self) -> int:
-        return self._highscore_newcomer
+        if self._highscore_newcomer is None:
+            self._highscore_newcomer = int(self._api_data.get("highscore_platz_newcomer"))
 
-    @highscore_newcomer.setter
-    def highscore_newcomer(self, value:int):
-        self._highscore_newcomer = value
+        return self._highscore_newcomer
 
     @property
     def agsx_punkte(self) -> int:
-        return self._agsx_punkte
+        if self._agsx_punkte is None:
+            self._agsx_punkte = int(self._api_data.get("agsx_punkte"))
 
-    @agsx_punkte.setter
-    def agsx_punkte(self, value:int):
-        self._agsx_punkte = value
+        return self._agsx_punkte
 
     @property
     def in_agsx(self) -> bool:
-        return self._in_agsx
+        if self._in_agsx is None:
+            self._in_agsx = self._api_data.get("in_agsx") == "true"
 
-    @in_agsx.setter
-    def in_agsx(self, value:bool):
-        self._in_agsx = value
+        return self._in_agsx
 
     @property
     def handelsaktivitaet(self) -> int:
-        return self._handelsaktivitaet
+        if self._handelsaktivitaet is None:
+            self._handelsaktivitaet = int(self._api_data.get("handelsaktivitaet"))
 
-    @handelsaktivitaet.setter
-    def handelsaktivitaet(self, value:int):
-        self._handelsaktivitaet = value
+        return self._handelsaktivitaet
 
     @property
     def ceo(self) -> Ceo:
-        return self._ceo
+        if self._ceo is None:
+            ceo_data:dict = self._api_data.get("ceo")
+            self._ceo = Ceo(name=ceo_data.get("name"),
+                            registrierung_datum=datetime.strptime(ceo_data.get("registrierung_datum"), "%Y-%m-%d %H:%M:%S"),
+                            gesperrt=ceo_data.get("gesperrt")=="true", userprojekt=ceo_data.get("ist_userprojekt_account")=="true")
 
-    @ceo.setter
-    def ceo(self, value:Ceo):
-        self._ceo = value
+        return self._ceo
 
     @property
     def aktien(self) -> list:
-        if type(self._aktien[0]) is not Aktie:
-            temp_list = []
+        if self._aktien is None:
+            self._aktien = []
             aktie: dict
-            for aktie in self._aktien:
+            for aktie in self._api_data.get("aktien"):
                 temp = Aktie(wkn=int(aktie.get("wkn")), stueckzahl=int(aktie.get("stueckzahl")))
-                temp_list.append(temp)
-            self._aktien = temp_list
+                self._aktien.append(temp)
 
         return self._aktien
 
-    @aktien.setter
-    def aktien(self, value:list):
-        self._aktien = value
-
     @property
     def anleihen(self) -> list:
-        if type(self._anleihen[0]) is not Anleihe:
-            temp_list = []
+        if self._anleihen is None:
+            self._anleihen = []
             anleihe: dict
-            for anleihe in self._anleihen:
+            for anleihe in self._api_data.get("anleihen"):
                 temp = Anleihe(betrag=int(anleihe.get("betrag")), zins=float(anleihe.get("zins")),
                                auszahlung_datum=datetime.strptime(anleihe.get("auszahlung_datum"), "%Y-%m-%d %H:%M:%S"),
                                laufzeit=int(anleihe.get("laufzeit")))
-                temp_list.append(temp)
-            self._anleihen = temp_list
+                self._anleihen.append(temp)
 
         return self._anleihen
 
-    @anleihen.setter
-    def anleihen(self, value:list):
-        self._anleihen = value
-
     @property
     def kredite(self) -> list:
-        if type(self._kredite[0]) is not Kredit:
-            temp_list = []
+        if self._kredite is None:
+            self._kredite = []
             kredit: dict
-            for kredit in self._kredite:
+            for kredit in self._api_data.get("kredite"):
                 temp = Kredit(betrag=int(kredit.get("betrag")), zins=float(kredit.get("zins")),
-                               rueckzahlung_datum=datetime.strptime(kredit.get("auszahlung_datum"), "%Y-%m-%d %H:%M:%S"),
-                               laufzeit=int(kredit.get("laufzeit")))
-                temp_list.append(temp)
-            self._kredite = temp_list
+                              rueckzahlung_datum=datetime.strptime(kredit.get("rueckzahlung_datum"), "%Y-%m-%d %H:%M:%S"),
+                              laufzeit=int(kredit.get("laufzeit")))
+                self._kredite.append(temp)
 
         return self._kredite
 
-    @kredite.setter
-    def kredite(self, value:list):
-        self._kredite = value
-
     @property
     def zertifikate(self) -> list:
-        if type(self._zertifikate[0]) is not Zertifikat:
-            temp_list = []
+        if self._zertifikate is None:
+            self._zertifikate = []
             zertifikat: dict
-            for zertifikat in self._zertifikate:
+            for zertifikat in self._api_data.get("zertifikate"):
                 temp = Zertifikat(betrag=float(zertifikat.get("betrag")), typ=zertifikat.get("typ"),
                                   hebel=float(zertifikat.get("hebel")), punkte=int(zertifikat.get("punkte")),
                                   ablaufdatum=datetime.strptime(zertifikat.get("ablauf_datum"), "%Y-%m-%d %H:%M:%S"))
-                temp_list.append(temp)
-            self._zertifikate = temp_list
+                self._zertifikate.append(temp)
 
         return self._zertifikate
 
-    @zertifikate.setter
-    def zertifikate(self, value:list):
-        self._zertifikate = value
-
     @property
     def orders(self) -> list:
-        if type(self._orders[0]) is not Order:
-            temp_list = []
+        if self._orders is None:
+            self._orders = []
             order: dict
-            for order in self._orders:
-                temp = Order(typ=order.get("typ"), limit=float(order.get("limit")), stueckzahl=int(order.get("stueckzahl")),
-                             orderregel=order.get("orderregel")=="true", systembank=order.get("systembank_order")=="true",
+            for order in self._api_data.get("orders"):
+                temp = Order(typ=order.get("typ"), limit=float(order.get("limit")),
+                             stueckzahl=int(order.get("stueckzahl")),
+                             orderregel=order.get("orderregel") == "true",
+                             systembank=order.get("systembank_order") == "true",
                              datum=datetime.strptime(order.get("datum"), "%Y-%m-%d %H:%M:%S"))
-                temp_list.append(temp)
-            self._orders = temp_list
+                self._orders.append(temp)
 
         return self._orders
-
-    @orders.setter
-    def orders(self, value:list):
-        self._orders = value
