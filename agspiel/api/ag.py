48e421aef4399a1ec5dce5719804aac7c84d0038
KingKevin23 <kevin.baier@protonmail.com>
2020-08-11 21:09:17 +0200

    readded setter and removed init params

--- agspiel/api/ag.py
+++ agspiel/api/ag.py
@@ -10,45 +10,43 @@ from .zertifikat import Zertifikat
 from .order import Order
 
 class Ag:
-    def __init__(self, api_data:dict, web_data:bytes):
-        self._api_data:dict = api_data
-        self._web_data:BeautifulSoup = BeautifulSoup(web_data, "html.parser")
-        self._wkn = None
-        self._name = None
-        self._gruendung = None
-        self._aktienanzahl = None
-        self._in_liquidation = None
-        self._kurs = None
-        self._brief = None
-        self._geld = None
-        self._brief_stueckzahl = None
-        self._geld_stueckzahl = None
-        self._sw_aktie = None
-        self._bbw_aktie = None
-        self._fp_aktie = None
-        self._bw_aktie = None
-        self._kurs_14d = None
+    def __init__(self):
+        self.wkn = None
+        self.name = None
+        self.gruendung = None
+        self.aktienanzahl = None
+        self.in_liquidation = None
+        self.kurs = None
+        self.brief = None
+        self.geld = None
+        self.brief_stueckzahl = None
+        self.geld_stueckzahl = None
+        self.sw_aktie = None
+        self.bbw_aktie = None
+        self.fp_aktie = None
+        self.bw_aktie = None
+        self.kurs_14d = None
         #TODO: self._kgv = None
         #TODO: self._spread = None
         #TODO: self._alter = None
         #TODO: self._tagesvolumen = None
         #TODO: self._boersenwert = None
-        self._buchwert = None
-        self._depotwert = None
-        self._bargeld = None
-        self._highscore = None
-        self._highscore_groesse = None
-        self._highscore_wachstum = None
-        self._highscore_newcomer = None
-        self._agsx_punkte = None
-        self._in_agsx = None
-        self._handelsaktivitaet = None
-        self._ceo = None
-        self._aktien = None
-        self._anleihen = None
-        self._kredite = None
-        self._zertifikate = None
-        self._orders = None
+        self.buchwert = None
+        self.depotwert = None
+        self.bargeld = None
+        self.highscore = None
+        self.highscore_groesse = None
+        self.highscore_wachstum = None
+        self.highscore_newcomer = None
+        self.agsx_punkte = None
+        self.in_agsx = None
+        self.handelsaktivitaet = None
+        self.ceo = None
+        self.aktien = None
+        self.anleihen = None
+        self.kredite = None
+        self.zertifikate = None
+        self.orders = None
 
     @property
     def wkn(self) -> int:
@@ -57,12 +55,20 @@ class Ag:
 
         return self._wkn
 
+    @wkn.setter
+    def wkn(self, value:int):
+        self._wkn = value
+
     @property
     def name(self) -> str:
         if self._name is None:
             self._name = self._api_data.get("name")
 
         return self._name
+
+    @name.setter
+    def name(self, value:str):
+        self._name = value
 
     @property
     def gruendung(self) -> datetime:
@@ -71,12 +77,20 @@ class Ag:
 
         return self._gruendung
 
+    @gruendung.setter
+    def gruendung(self, value:datetime):
+        self._gruendung = value
+
     @property
     def aktienanzahl(self) -> int:
         if self._aktienanzahl is None:
             self._aktienanzahl = int(self._api_data.get("aktienanzahl"))
 
         return self._aktienanzahl
+
+    @aktienanzahl.setter
+    def aktienanzahl(self, value:int):
+        self._aktienanzahl = value
 
     @property
     def in_liquidation(self) -> bool:
@@ -85,12 +99,20 @@ class Ag:
 
         return self._in_liquidation
 
+    @in_liquidation.setter
+    def in_liquidation(self, value:bool):
+        self._in_liquidation = value
+
     @property
     def kurs(self) -> float:
         if self._kurs is None:
             self._kurs = float(self._api_data.get("kurs"))
 
         return self._kurs
+
+    @kurs.setter
+    def kurs(self, value:float):
+        self._kurs = value
 
     @property
     def brief(self) -> float:
@@ -99,12 +121,20 @@ class Ag:
 
         return self._brief
 
+    @brief.setter
+    def brief(self, value:float):
+        self._brief = value
+
     @property
     def geld(self) -> float:
         if self._geld is None:
             self._geld = float(self._api_data.get("geld"))
 
         return self._geld
+
+    @geld.setter
+    def geld(self, value:float):
+        self._geld = value
 
     @property
     def brief_stueckzahl(self) -> int:
@@ -113,12 +143,20 @@ class Ag:
 
         return self._brief_stueckzahl
 
+    @brief_stueckzahl.setter
+    def brief_stueckzahl(self, value:int):
+        self._brief_stueckzahl = value
+
     @property
     def geld_stueckzahl(self) -> int:
         if self._geld_stueckzahl is None:
             self._geld_stueckzahl = int(self._api_data.get("geld_stueckzahl"))
 
         return self._geld_stueckzahl
+
+    @geld_stueckzahl.setter
+    def geld_stueckzahl(self, value:int):
+        self._geld_stueckzahl = value
 
     @property
     def sw_aktie(self) -> float:
@@ -129,6 +167,10 @@ class Ag:
 
         return self._sw_aktie
 
+    @sw_aktie.setter
+    def sw_aktie(self, value:float):
+        self._sw_aktie = value
+
     @property
     def bbw_aktie(self) -> float:
         if self._bbw_aktie is None:
@@ -137,6 +179,10 @@ class Ag:
             self._bbw_aktie = float(self._bbw_aktie.replace(".", "").replace(",", "."))
 
         return self._bbw_aktie
+
+    @bbw_aktie.setter
+    def bbw_aktie(self, value:float):
+        self._bbw_aktie = value
 
     @property
     def fp_aktie(self) -> float:
@@ -147,12 +193,20 @@ class Ag:
 
         return self._fp_aktie
 
+    @fp_aktie.setter
+    def fp_aktie(self, value:float):
+        self._fp_aktie = value
+
     @property
     def bw_aktie(self) -> float:
         if self._bw_aktie is None:
             self._bw_aktie = round(self.buchwert / self.aktienanzahl, 2)
 
         return self._bw_aktie
+
+    @bw_aktie.setter
+    def bw_aktie(self, value:float):
+        self._bw_aktie = value
 
     @property
     def kurs_14d(self) -> float:
@@ -163,12 +217,20 @@ class Ag:
 
         return self._kurs_14d
 
+    @kurs_14d.setter
+    def kurs_14d(self, value: str):
+        self._kurs_14d = value
+
     @property
     def buchwert(self) -> float:
         if self._buchwert is None:
             self._buchwert = self.depotwert + self.bargeld
 
         return self._buchwert
+
+    @buchwert.setter
+    def buchwert(self, value:float):
+        self._buchwert = value
 
     @property
     def depotwert(self) -> float:
@@ -177,12 +239,20 @@ class Ag:
 
         return self._depotwert
 
+    @depotwert.setter
+    def depotwert(self, value:float):
+        self._depotwert = value
+
     @property
     def bargeld(self) -> float:
         if self._bargeld is None:
             self._bargeld = float(self._api_data.get("bargeld"))
 
         return self._bargeld
+
+    @bargeld.setter
+    def bargeld(self, value:float):
+        self._bargeld = value
 
     @property
     def highscore(self) -> int:
@@ -191,12 +261,20 @@ class Ag:
 
         return self._highscore
 
+    @highscore.setter
+    def highscore(self, value:int):
+        self._highscore = value
+
     @property
     def highscore_groesse(self) -> int:
         if self._highscore_groesse is None:
             self._highscore_groesse = int(self._api_data.get("highscore_platz_groesse"))
 
         return self._highscore_groesse
+
+    @highscore_groesse.setter
+    def highscore_groesse(self, value:int):
+        self._highscore_groesse = value
 
     @property
     def highscore_wachstum(self) -> int:
@@ -205,12 +283,20 @@ class Ag:
 
         return self._highscore_wachstum
 
+    @highscore_wachstum.setter
+    def highscore_wachstum(self, value:int):
+        self._highscore_wachstum = value
+
     @property
     def highscore_newcomer(self) -> int:
         if self._highscore_newcomer is None:
             self._highscore_newcomer = int(self._api_data.get("highscore_platz_newcomer"))
 
         return self._highscore_newcomer
+
+    @highscore_newcomer.setter
+    def highscore_newcomer(self, value:int):
+        self._highscore_newcomer = value
 
     @property
     def agsx_punkte(self) -> int:
@@ -219,6 +305,10 @@ class Ag:
 
         return self._agsx_punkte
 
+    @agsx_punkte.setter
+    def agsx_punkte(self, value:int):
+        self._agsx_punkte = value
+
     @property
     def in_agsx(self) -> bool:
         if self._in_agsx is None:
@@ -226,12 +316,20 @@ class Ag:
 
         return self._in_agsx
 
+    @in_agsx.setter
+    def in_agsx(self, value:bool):
+        self._in_agsx = value
+
     @property
     def handelsaktivitaet(self) -> int:
         if self._handelsaktivitaet is None:
             self._handelsaktivitaet = int(self._api_data.get("handelsaktivitaet"))
 
         return self._handelsaktivitaet
+
+    @handelsaktivitaet.setter
+    def handelsaktivitaet(self, value:int):
+        self._handelsaktivitaet = value
 
     @property
     def ceo(self) -> Ceo:
@@ -245,6 +343,10 @@ class Ag:
 
         return self._ceo
 
+    @ceo.setter
+    def ceo(self, value:Ceo):
+        self._ceo = value
+
     @property
     def aktien(self) -> list:
         if self._aktien is None:
@@ -255,6 +357,10 @@ class Ag:
                 self._aktien.append(temp)
 
         return self._aktien
+
+    @aktien.setter
+    def aktien(self, value:list):
+        self._aktien = value
 
     @property
     def anleihen(self) -> list:
@@ -269,6 +375,10 @@ class Ag:
 
         return self._anleihen
 
+    @anleihen.setter
+    def anleihen(self, value:list):
+        self._anleihen = value
+
     @property
     def kredite(self) -> list:
         if self._kredite is None:
@@ -282,6 +392,10 @@ class Ag:
 
         return self._kredite
 
+    @kredite.setter
+    def kredite(self, value:list):
+        self._kredite = value
+
     @property
     def zertifikate(self) -> list:
         if self._zertifikate is None:
@@ -294,6 +408,10 @@ class Ag:
                 self._zertifikate.append(temp)
 
         return self._zertifikate
+
+    @zertifikate.setter
+    def zertifikate(self, value:list):
+        self._zertifikate = value
 
     @property
     def orders(self) -> list:
@@ -309,3 +427,7 @@ class Ag:
                 self._orders.append(temp)
 
         return self._orders
+
+    @orders.setter
+    def orders(self, value:list):
+        self._orders = value
