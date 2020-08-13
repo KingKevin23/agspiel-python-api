cba16b2bed4ffc126f58688bea29aba743b33d4b
KingKevin23 <kevin.baier@protonmail.com>
2020-08-13 18:50:06 +0200

    changed version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="agspiel-python-api",
-    version="0.1.0",
+    version="0.2.0",
     author="KingKevin23",
     author_email="code@kingkevin.de",
     description="Python API für das AG-Spiel",
