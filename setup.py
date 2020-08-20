8413aa18bb94d8423d60538c84341f2635a69cfd
KingKevin23 <kevin.baier@protonmail.com>
2020-08-20 15:26:47 +0200

    fixed pypi.org entry

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="agspiel-python-api",
-    version="0.3.0",
+    version="0.3.1",
     author="KingKevin23",
     author_email="code@kingkevin.de",
     description="Python API für das AG-Spiel",
