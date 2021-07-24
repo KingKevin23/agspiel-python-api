8fe3bb95c95f698b74f9fd3ef52c782eb356ab23
Kevin Baier <kevin.baier@protonmail.com>
2021-07-24 18:40:22 +0200

    added first basic version of historic data

---
+++ agspiel/api/historic_ag.py
@@ -0,0 +1,236 @@
+#  Copyright (c) 2021 | KingKevin23 (@kingkevin023)
+
+from datetime import datetime, date
+
+class HistorischeAg:
+
+    # Currently not included stats:
+    # UserID, IndexID, Premiumstatus, Übernahmeschutz, AGSXDurchschnittsplatzierung, Wachstumsdurchschnittsplatz
+
+    def __init__(self, wkn:int, datum:date, api_data:dict):
+        self._wkn:int = wkn
+        self._datum:date = datum
+        self._api_data:dict = api_data
+
+    @property
+    def wkn(self) -> int:
+        return self._wkn
+
+    @property
+    def datum(self) -> date:
+        return self._datum
+
+    @property
+    def name(self) -> str:
+        return self._api_data.get("AGName")
+
+    @property
+    def gruendung(self) -> datetime:
+        return datetime.strptime(self._api_data.get("Gruendungsdatum"), "%Y-%m-%d")
+
+    @property
+    def aktienanzahl(self) -> int:
+        return self._api_data.get("Aktienanzahl")
+
+    @property
+    def in_liquidation(self) -> bool:
+        raise NotImplementedError()
+
+    @property
+    def kurs(self) -> float:
+        return self._api_data.get("Kurs_Wert")
+
+    @property
+    def brief(self) -> float:
+        return self._api_data.get("Briefkurs_Wert")
+
+    @property
+    def geld(self) -> float:
+        return self._api_data.get("Geldkurs_Wert")
+
+    @property
+    def brief_stueckzahl(self) -> int:
+        return int(self._api_data.get("Briefvolumen"))
+
+    @property
+    def geld_stueckzahl(self) -> int:
+        return int(self._api_data.get("Geldvolumen"))
+
+    @property
+    def sw_aktie(self) -> float:
+        return self._api_data.get("SW_Wert")
+
+    @property
+    def bbw_aktie(self) -> float:
+        return self._api_data.get("BBW_Wert")
+
+    @property
+    def fp_aktie(self) -> float:
+        return self._api_data.get("FP_Wert")
+
+    @property
+    def bw_aktie(self) -> float:
+        return self._api_data.get("BW_Wert")
+
+    @property
+    def kgv(self) -> float:
+        raise NotImplementedError()
+
+    @property
+    def spread(self) -> float:
+        if self.geld == 0 or self.brief == 0:
+            return 0
+        else:
+            return 1 - (self.geld / self.brief)
+
+    @property
+    def alter(self) -> int:
+        return (datetime.now() - self.gruendung).days
+
+    @property
+    def tagesvolumen(self) -> float:
+        raise NotImplementedError()
+
+    @property
+    def boersenwert(self) -> float:
+        return self.kurs * self.aktienanzahl
+
+    @property
+    def buchwert(self) -> float:
+        return self.bargeld + self.depotwert
+
+    @property
+    def depotwert(self) -> float:
+        return self._api_data.get("Aktienbuchwert_Wert") * self._api_data.get("Anleihen_Wert") * self._api_data.get("Zertifikate_Wert")
+
+    @property
+    def bargeld(self) -> float:
+        return self._api_data.get("Cash_Wert")
+
+    @property
+    def highscore(self) -> int:
+        return self._api_data.get("Platz")
+
+    @property
+    def highscore_groesse(self) -> int:
+        return self._api_data.get("Groessenplatz")
+
+    @property
+    def highscore_wachstum(self) -> int:
+        return self._api_data.get("Wachstumsplatz")
+
+    @property
+    def highscore_newcomer(self) -> int:
+        raise NotImplementedError()
+
+    @property
+    def agsx_punkte(self) -> int:
+        return self._api_data.get("AGSXPunkte")
+
+    @property
+    def in_agsx(self) -> bool:
+        return self.highscore_wachstum <= 10 or self.highscore_groesse <= 10
+
+    @property
+    def handelsaktivitaet(self) -> int:
+        return int(self._api_data.get("Aktivitaet"))
+
+    @property
+    def ceo(self):
+        raise NotImplementedError()
+
+    @property
+    def aktien(self):
+        raise NotImplementedError()
+
+    @property
+    def anleihen(self):
+        raise NotImplementedError()
+
+    @property
+    def kredite(self):
+        raise NotImplementedError()
+
+    @property
+    def zertifikate(self):
+        raise NotImplementedError()
+
+    @property
+    def orders(self):
+        raise NotImplementedError()
+
+    @property
+    def aktionaere(self):
+        raise NotImplementedError()
+
+    @property
+    def kes(self):
+        raise NotImplementedError()
+
+    @property
+    def khs(self):
+        raise NotImplementedError()
+
+    @property
+    def dividende(self) -> float:
+        raise NotImplementedError()
+
+    @property
+    def max_zertis(self) -> int:
+        raise NotImplementedError()
+
+    @property
+    def tages_hoch(self) -> float:
+        raise NotImplementedError()
+
+    @property
+    def tages_tief(self) -> float:
+        raise NotImplementedError()
+
+    @property
+    def kurs_14d(self) -> float:
+        pass
+
+    @property
+    def kurs_30d(self) -> float:
+        pass
+
+    @property
+    def kurs_60d(self) -> float:
+        pass
+
+    @property
+    def kurs_90d(self) -> float:
+        pass
+
+    @property
+    def bw_14d(self) -> float:
+        pass
+
+    @property
+    def bw_30d(self) -> float:
+        pass
+
+    @property
+    def bw_60d(self) -> float:
+        pass
+
+    @property
+    def bw_90d(self) -> float:
+        pass
+
+    @property
+    def fp_14d(self) -> float:
+        pass
+
+    @property
+    def fp_30d(self) -> float:
+        pass
+
+    @property
+    def fp_60d(self) -> float:
+        pass
+
+    @property
+    def fp_90d(self) -> float:
+        pass
---
+++ tests/test_historic_ag.py
@@ -0,0 +1,205 @@
+#  Copyright (c) 2021 | KingKevin23 (@kingkevin023)
+
+from unittest import TestCase
+from datetime import date, datetime
+from json import loads
+
+from agspiel.api.historic_ag import HistorischeAg
+
+class TestHistorischeAg(TestCase):
+    def setUp(self):
+        f = open("testhistoricag.json", "r")
+        api_data = loads(f.read())
+        f.close()
+        self.ag = HistorischeAg(wkn=175353, datum=date(2021, 7, 23), api_data=api_data)
+
+    def test_wkn(self):
+        self.assertEqual(self.ag.wkn, 175353)
+        self.assertIsInstance(self.ag.wkn, int)
+
+    def test_datum(self):
+        self.assertEqual(self.ag.datum, date(2021, 7, 23))
+        self.assertIsInstance(self.ag.datum, date)
+
+    def test_name(self):
+        self.assertEqual(self.ag.name, "King Kompany")
+        self.assertIsInstance(self.ag.name, str)
+
+    def test_gruendung(self):
+        self.assertEqual(self.ag.gruendung, datetime(year=2020, month=2, day=4))
+        self.assertIsInstance(self.ag.gruendung, datetime)
+
+    def test_aktienanzahl(self):
+        self.assertEqual(self.ag.aktienanzahl, 4000000)
+        self.assertIsInstance(self.ag.aktienanzahl, int)
+
+    def test_in_liquidation(self):
+        self.fail()
+
+    def test_kurs(self):
+        self.assertEqual(self.ag.kurs, 630)
+        self.assertIsInstance(self.ag.kurs, float)
+
+    def test_brief(self):
+        self.assertEqual(self.ag.brief, 630)
+        self.assertIsInstance(self.ag.brief, float)
+
+    def test_geld(self):
+        self.assertEqual(self.ag.geld, 601.99)
+        self.assertIsInstance(self.ag.geld, float)
+
+    def test_brief_stueckzahl(self):
+        self.assertEqual(self.ag.brief_stueckzahl, 16287)
+        self.assertIsInstance(self.ag.brief_stueckzahl, int)
+
+    def test_geld_stueckzahl(self):
+        self.assertEqual(self.ag.geld_stueckzahl, 10945)
+        self.assertIsInstance(self.ag.geld_stueckzahl, int)
+
+    def test_sw_aktie(self):
+        self.assertEqual(self.ag.sw_aktie, 352.26)
+        self.assertIsInstance(self.ag.sw_aktie, float)
+
+    def test_bbw_aktie(self):
+        self.assertEqual(self.ag.bbw_aktie, 482.78)
+        self.assertIsInstance(self.ag.bbw_aktie, float)
+
+    def test_fp_aktie(self):
+        self.assertEqual(self.ag.fp_aktie, 436.23)
+        self.assertIsInstance(self.ag.fp_aktie, float)
+
+    def test_bw_aktie(self):
+        self.assertEqual(self.ag.bw_aktie, 114.47)
+        self.assertIsInstance(self.ag.bw_aktie, float)
+
+    def test_kgv(self):
+        self.fail()
+
+    def test_spread(self):
+        self.assertEqual(self.ag.spread, 0.04446031746031742)
+        self.assertIsInstance(self.ag.spread, float)
+
+    def test_alter(self):
+        self.assertEqual(self.ag.alter, (datetime.now() - self.ag.gruendung).days)
+        self.assertIsInstance(self.ag.alter, int)
+
+    def test_tagesvolumen(self):
+        self.fail()
+
+    def test_boersenwert(self):
+        self.assertEqual(self.ag.boersenwert, 2520000000)
+        self.assertIsInstance(self.ag.boersenwert, float)
+
+    def test_buchwert(self):
+        self.assertEqual(self.ag.buchwert, 2348360222.54)
+        self.assertIsInstance(self.ag.buchwert, float)
+
+    def test_depotwert(self):
+        self.assertEqual(self.ag.depotwert, 1856199270.98)
+        self.assertIsInstance(self.ag.depotwert, float)
+
+    def test_bargeld(self):
+        self.assertEqual(self.ag.bargeld, 492160951.56)
+        self.assertIsInstance(self.ag.bargeld, float)
+
+    def test_highscore(self):
+        self.assertEqual(self.ag.highscore, 49)
+        self.assertIsInstance(self.ag.highscore, int)
+
+    def test_highscore_groesse(self):
+        self.assertEqual(self.ag.highscore_groesse, 178)
+        self.assertIsInstance(self.ag.highscore_groesse, int)
+
+    def test_highscore_wachstum(self):
+        self.assertEqual(self.ag.highscore_wachstum, 174)
+        self.assertIsInstance(self.ag.highscore_wachstum, int)
+
+    def test_highscore_newcomer(self):
+        self.fail()
+
+    def test_agsx_punkte(self):
+        self.assertEqual(self.ag.agsx_punkte, 1302)
+        self.assertIsInstance(self.ag.agsx_punkte, int)
+
+    def test_in_agsx(self):
+        self.assertEqual(self.ag.in_agsx, False)
+        self.assertIsInstance(self.ag.in_agsx, bool)
+
+    def test_handelsaktivitaet(self):
+        self.assertEqual(self.ag.handelsaktivitaet, 94)
+        self.assertIsInstance(self.ag.handelsaktivitaet, int)
+
+    def test_ceo(self):
+        self.fail()
+
+    def test_aktien(self):
+        self.fail()
+
+    def test_anleihen(self):
+        self.fail()
+
+    def test_kredite(self):
+        self.fail()
+
+    def test_zertifikate(self):
+        self.fail()
+
+    def test_orders(self):
+        self.fail()
+
+    def test_aktionaere(self):
+        self.fail()
+
+    def test_kes(self):
+        self.fail()
+
+    def test_khs(self):
+        self.fail()
+
+    def test_dividende(self):
+        self.fail()
+
+    def test_max_zertis(self):
+        self.fail()
+
+    def test_tages_hoch(self):
+        self.fail()
+
+    def test_tages_tief(self):
+        self.fail()
+
+    def test_kurs_14d(self):
+        self.fail()
+
+    def test_kurs_30d(self):
+        self.fail()
+
+    def test_kurs_60d(self):
+        self.fail()
+
+    def test_kurs_90d(self):
+        self.fail()
+
+    def test_bw_14d(self):
+        self.fail()
+
+    def test_bw_30d(self):
+        self.fail()
+
+    def test_bw_60d(self):
+        self.fail()
+
+    def test_bw_90d(self):
+        self.fail()
+
+    def test_fp_14d(self):
+        self.fail()
+
+    def test_fp_30d(self):
+        self.fail()
+
+    def test_fp_60d(self):
+        self.fail()
+
+    def test_fp_90d(self):
+        self.fail()
